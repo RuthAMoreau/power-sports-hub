ca972147f334b1841051991550eb632c5b9e0fc0
RuthAMoreau <ram15k@fsu.edu>
2026-07-29 21:42:33 -0500

    Update app.js

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const session = require("express-session");
+const methodOverride = require("method-override");
+const path = require("path");
+require("dotenv").config();
+
+const app = express();
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(methodOverride("_method"));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || "powersportshubsecret",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+// View Engine
+app.set("view engine", "ejs");
+
+// Home Route
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+// Start Server
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
