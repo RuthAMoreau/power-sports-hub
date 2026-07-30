6b6c0b2ba02b4eaaa244c1251c446cd6ff5b7c06
RuthAMoreau <ram15k@fsu.edu>
2026-07-29 22:42:32 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ require('@dotenvx/dotenvx').config(); // or require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
-const express = require("express");
 const session = require("express-session");
 const methodOverride = require("method-override");
 const path = require("path");
